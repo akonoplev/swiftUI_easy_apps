d671bb262359341cbfcb7f934b63d9e8cfeebf47
akonoplev <bringmethedream@mail.ru>
2022-12-06 16:25:58 +1100

    guees the flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Андрей Коноплев on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
